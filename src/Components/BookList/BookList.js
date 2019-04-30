7f26e41924eff9aa50333566e0e3d6706411df21
Nigel Jones <nigejones@gmail.com>
2019-04-30 19:50:44 +0100

    Styling

--- src/Components/BookList/BookList.js
+++ src/Components/BookList/BookList.js
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { observer, inject } from "mobx-react";
 
+import './BookList.scss';
+
 @inject('store')
 @observer
 class BookList extends React.Component {
   render() {
     return (
-      <div>
-        { this.props.store.books.map(book => <div id={book.id}>{book.title}</div>) }
-      </div>
+      <>
+        <h1>The Book List</h1>
+        <div className="booklist">
+          { this.props.store.books.map(book => <div id={book.id}>{book.title} by {book.author}</div>) }
+        </div>
+      </>
     )
   }
 }
